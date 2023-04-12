9648556d371ab6c0520c31f63e02dddc6a2ca7e7
lmac-1 <macartney.lucy@gmail.com>
2023-04-12 14:44:35 -0500

    fixes countdown console error

--- components/Countdown.js
+++ components/Countdown.js
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Countdown = ({ seconds = 3, setStep }) => {
   const [count, setCount] = useState(seconds);
 
-  const countdown = () => {
-    setTimeout(() => {
-      setCount(count - 1);
+  useEffect(() => {
+    if (count === 0) {
+      setStep("game");
+      return;
+    }
+
+    const countdownInterval = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
     }, 1000);
-  };
 
-  if (count > 0) {
-    countdown();
-  }
-
-  if (count === 0) {
-    setStep("game");
-  }
+    return () => {
+      clearInterval(countdownInterval);
+    };
+  }, [count, setStep]);
 
   return (
     <div className="text-[12rem] text-lightning-green font-bold">{count}</div>
